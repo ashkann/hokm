bf3da8bad170749a296a0b9e0b033cdc5aeb769b
mehdi <mehdigarakani@gmail.com>
2015-10-05 19:51:32 +0330

    Suite primary constructor is private, thus no other value (except those in the companion object) are possible

--- src/main/scala/ir/ashkan/hokm/Suite.scala
+++ src/main/scala/ir/ashkan/hokm/Suite.scala
@@ -1,14 +1,9 @@
 package ir.ashkan.hokm
 
-sealed abstract class Suite(val symbol:Char) {
+sealed abstract class Suite private (val symbol:Char) {
   override def toString = s"$symbol"
   val cards: Set[Card] = Rank.ranks.map { _ of this }
 }
-
-//object Hearts extends Suite('\u2665')
-//object Spades extends Suite('\u2660')
-//object Clubs extends Suite('\u2663')
-//object Diamonds extends Suite('\u2666')
 
 object Suite {
   object Hearts extends Suite('\u2665')
